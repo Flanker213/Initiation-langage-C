f4c6a642931c177183f4e86dc6c57525ba75bc4e
Amine <assas-a@outlook.fr>
2022-02-23 01:33:47 +0100

    Les boucles

---
+++ lesboucles/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Une boucle est une technique qui permet de répéter les mêmes instructions plusieurs fois
+
+Comme les conditions il y a plusieurs façon de réaliser des boucles :
+
+1 - while
+2 - do.. while
+3 - for
+
+Dans tous les cas le principe est le même :
+
+1 - L'ordinateur lit les instruction
+2 - Une fois arrivé à la fin de la boucle il repart à la première instruction
+3 - Il recommence à lire les instructions
+4 - Il repart au debut de la boucle
+
+
+*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
